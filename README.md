51f9cf98876a4af791368c36fa00fc005b1c87de
xnought <bertuccd@oregonstate.edu>
2023-04-29 17:08:50 -0700

    docs: document how to use the api

--- README.md
+++ README.md
@@ -13,3 +13,32 @@ Then run the REST server with
 ```bash
 python3 main.py
 ```
+
+which hosts the only two endpoint `/convert` and `/supported-currencies` on `http://localhost:8000`.
+
+### `http://localhost:8000/supported-currencies`
+
+GET request to `/supported-currencies` returns a list of supported currencies.
+
+```bash
+curl http://localhost:8000/supported-currencies
+# >>> ["CAD","USD","EUR","JPY","CNY","GBP","AUD","CHF"]
+```
+
+### `http://localhost:8000/convert/{from}/{amount}/{to}`
+
+GET request that converts a supported currency name of a given amount to a different supported currency.
+
+For example, I want to convert 100 US Dollars (USD) to canadian (CAD).
+
+```bash
+curl http://localhost:8000/convert/USD/100/CAD
+# >>> {"amount":133.6217102267553,"currency":"CAD"}
+```
+
+For example, I want to convert 348.55 canadian (CAD) to euros (EUR)
+
+```bash
+curl http://localhost:8000/convert/CAD/348.55/EUR
+# >>> {"amount":237.54515095754107,"currency":"EUR"}
+```
--- main.py
+++ main.py
@@ -1,13 +1,49 @@
 from fastapi import FastAPI
 from uvicorn import run
 from pydantic import BaseModel
+from fastapi.exceptions import HTTPException
+from currency_converter import CurrencyConverter
 
 app = FastAPI()
+c = CurrencyConverter()
 
 
-@app.get("/")
-def index():
-    return {"message": "Hello, world!"}
+class ConvertResponse(BaseModel):
+    amount: float
+    currency: str
+
+
+SUPPORTED_CURRENCIES = [
+    "CAD",  # Canadian Dollar
+    "USD",  # US Dollar
+    "EUR",  # Euro
+    "JPY",  # Japanese Yen
+    "CNY",  # Chinese Yuan Renminbi
+    "GBP",  # British Pound
+    "AUD",  # Australian Dollar
+    "CHF",  # Swiss Franc
+]
+
+
+@app.get("/supported-currencies")
+def supported_currencies():
+    return SUPPORTED_CURRENCIES
+
+
+@app.get("/convert/{from_currency:str}/{amount:float}/{to_currency:str}")
+def convert(from_currency: str, amount: float, to_currency: str) -> ConvertResponse:
+    if (
+        from_currency not in SUPPORTED_CURRENCIES
+        or to_currency not in SUPPORTED_CURRENCIES
+    ):
+        raise HTTPException(status_code=404, detail="Currency not found")
+
+    try:
+        conversion = c.convert(amount, from_currency, to_currency)
+    except ValueError:
+        raise HTTPException(status_code=404, detail="Converter failed")
+
+    return ConvertResponse(amount=conversion, currency=to_currency)
 
 
 if __name__ == "__main__":
